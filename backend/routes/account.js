6aae200e4eadf11fd3e977ffba4edf5df10a0e5d
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-03-17 15:20:12 +0530

    added transfer route

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { authmiddleware } = require('../middleware');
 const { Account } = require('../db');
+const { default: mongoose } = require('mongoose');
 
 const router = express.Router();
 
@@ -16,6 +17,39 @@ router.get('/balance',authmiddleware,async(req,res)=>{
   })
 })
 
+// transfer money
+router.post('/transfer',authmiddleware,async(req,res)=>{
+  const session = await mongoose.startSession();
 
+  session.startTransaction();
+  const {amount, to} = req.body;
+
+  const account = await Account.findOne({userid : req.userid}).session(session);
+
+  if(!account || account.balance < amount){
+    await session.abortTransaction();
+    res.status(400).json({
+      message: 'Insufficient balance'
+    })
+  }
+
+  const toaccount = await Account.findOne({userid: to}).session(session);
+
+  if(!toaccount){
+    res.status(400).json({
+      message: "Invalid account"
+    })
+  }
+
+  // transaction
+  await Account.updateOne({userid:req.userid},{$inc:{balance:-amount}}).session(session)
+  await Account.updateOne({userid:to},{$inc:{balance:amount}}).session(session)
+
+  await session.commitTransaction();
+  res.json({
+    message: 'Transfer completed'
+  })
+
+})
 
 module.exports = router;
