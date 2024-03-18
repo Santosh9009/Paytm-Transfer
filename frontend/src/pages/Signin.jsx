2133ad3cae846a116b13bb39f8dfff6084cf0b19
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-03-19 01:48:13 +0530

    added signin page

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -6,7 +6,14 @@ import Warning from "../components/Warning"
 const Signin = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-   
+           <div className="flex flex-col gap-5 w-96 text-center shadow-md p-5 rounded-lg">
+      <Header heading={'Sign in'} />
+      <Subheading Subheading={'Enter your credentials to access your account'} />
+      <Inputbox label={'Email'} placeholder={'jogndoe@gmail.com'}/>
+      <Inputbox label={'Password'} placeholder={''}/>
+      <Button title={'Sign in'}/>
+      <Warning text1={`Don't have an account?`} text2={'Sign Up'} to={'/signup'} />
+        </div>
       </div>
   )
 }
