62309c5a40f93b0e9ac1b69f8136d977f1fa48ab
Santosh Kumar Pati <patisantosh00@gmail.com>
2024-05-14 22:23:06 +0530

    bug fix

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -5,9 +5,9 @@ const Appbar = ({name}) => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full flex justify-between py-4 px-8 text-xl">
+    <div className="w-full flex items-start justify-between py-4 px-8 text-xl">
       <div className="font-medium text-2xl">Paymets App</div>
-      <div className="flex gap-3 font-medium">
+      <div className="flex items-center gap-3 font-medium">
         <div>Hello, {name}</div>
         <div className="flex justify-center items-center w-8 h-8 bg-blue-300 rounded-full">{name[0].toUpperCase()}</div>
         <Button onPress={()=>{
